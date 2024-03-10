9184bde82416f6a9500190d56ae2302d87ca97d8
Kinga Władzińska <58950222+Wladska@users.noreply.github.com>
2024-03-10 18:31:23 +0100

    Update build.gradle.kts to support sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,17 @@
 plugins {
     id("java")
+    id("org.sonarqube") version "4.4.1.3373"
 }
 
 group = "org.wladska"
 version = "1.0-SNAPSHOT"
+
+sonar {
+  properties {
+    property("sonar.projectKey", "CodeGenClash")
+    property("sonar.projectName", "CodeGenClash")
+  }
+}
 
 repositories {
     mavenCentral()
