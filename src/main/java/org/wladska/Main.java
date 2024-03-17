9d426044b54fe62eddc5586102e3481bb2a7713c
Popularkiya <kingawladzinsk@gmail.com>
2024-03-17 16:45:00 +0100

    easy/contains-duplicate/code-whisperer

--- src/main/java/org/wladska/Main.java
+++ src/main/java/org/wladska/Main.java
@@ -4,4 +4,35 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
+
+	/**
+	 * Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+	 * 
+	 * Example 1:
+	 * Input: nums = [1,2,3,1]
+	 * Output: true
+	 * 
+	 * Example 2:
+	 * Input: nums = [1,2,3,4]
+	 * Output: false
+	 * 
+	 * Example 3:
+	 * Input: nums = [1,1,1,3,3,4,3,2,4,2]
+	 * Output: true
+	 * 
+	 * Constraints:
+	 * 1 <= nums.length <= 10^5
+	 * -10^9 <= nums[i] <= 10^9
+	 */
+	public static boolean containsDuplicate(int[] nums) {
+		int[] array = new int[nums.length];
+		for (int i = 0; i < nums.length; i++) {
+			if (array[nums[i]] == 1) {
+				return true;
+			}
+			array[nums[i]]++;
+		}
+		return false;
+	}
+
 }
