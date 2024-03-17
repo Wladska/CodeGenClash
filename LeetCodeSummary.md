9e1c7ef6f8c0c355a71b4a38a91b161d9d316160
Popularkiya <kingawladzinsk@gmail.com>
2024-03-17 16:06:12 +0100

    easy/add-binary/code-whisperer

--- LeetCodeSummary.md
+++ LeetCodeSummary.md
@@ -1,6 +1,6 @@
 # CodeGenClash
 
-[ **place link to leet code after submit summary** ]
+https://leetcode.com/problems/add-binary/submissions/1206403068
 
 ![runtime](./images/leetcodesummary/runtime.png)
 
--- src/main/java/org/wladska/Main.java
+++ src/main/java/org/wladska/Main.java
@@ -4,4 +4,37 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
+
+	/**
+	 * Given two binary strings a and b, return their sum as a binary string.
+	 * 
+	 * Example 1:
+	 * Input: a = "11", b = "1"
+	 * Output: "100"
+	 * 
+	 * Example 2:
+	 * Input: a = "1010", b = "1011"
+	 * Output: "10101"
+	 * 
+	 * Constraints:
+	 * 1 <= a.length, b.length <= 104
+	 * a and b consist only of '0' or '1' characters.
+	 * Each string does not contain leading zeros except for the zero itself.
+	 */
+	public String addBinary(String a, String b) {
+		int carry = 0;
+		int i = a.length() - 1;
+		int j = b.length() - 1;
+		StringBuilder sb = new StringBuilder();
+		while (i >= 0 || j >= 0 || carry == 1) {
+			int aDigit = i >= 0 ? a.charAt(i) - '0' : 0;
+			int bDigit = j >= 0 ? b.charAt(j) - '0' : 0;
+			int sum = aDigit + bDigit + carry;
+			sb.append(sum % 2);
+			carry = sum / 2;
+			i--;
+			j--;
+		}
+		return sb.reverse().toString();
+	}
 }
