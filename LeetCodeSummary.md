60884f46dd66c02e838505c6acfa6c8e471ce573
Popularkiya <kingawladzinsk@gmail.com>
2024-03-17 16:51:38 +0100

    easy/valid-anagram/tabnine

--- LeetCodeSummary.md
+++ LeetCodeSummary.md
@@ -1,6 +1,6 @@
 # CodeGenClash
 
-[ **place link to leet code after submit summary** ]
+https://leetcode.com/problems/valid-anagram/submissions/1206435404
 
 ![runtime](./images/leetcodesummary/runtime.png)
 
--- src/main/java/org/wladska/Main.java
+++ src/main/java/org/wladska/Main.java
@@ -4,4 +4,40 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
+
+	/**
+	 * Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+	 * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+	 * 
+	 * Example 1:
+	 * Input: s = "anagram", t = "nagaram"
+	 * Output: true
+	 * 
+	 * Example 2:
+	 * Input: s = "rat", t = "car"
+	 * Output: false
+	 * 
+	 * Constraints:
+	 * 1 <= s.length, t.length <= 5 * 10^4
+	 * s and t consist of lowercase English letters.
+	 */
+	public boolean isAnagram(String s, String t) {
+		if (s.length()!= t.length()) {
+            return false;
+        }
+        int[] sCounts = new int[26];
+        int[] tCounts = new int[26];
+        for (char c : s.toCharArray()) {
+            sCounts[c - 'a']++;
+        }
+        for (char c : t.toCharArray()) {
+            tCounts[c - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (sCounts[i]!= tCounts[i]) {
+                return false;
+            }
+        }
+        return true;
+	}
 }
