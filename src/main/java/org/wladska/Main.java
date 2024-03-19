ae53836e26c79f7e26303b1173aabce8f9368fae
Popularkiya <kingawladzinsk@gmail.com>
2024-03-19 16:43:45 +0100

    medium/integer-to-roman/tabnine

--- src/main/java/org/wladska/Main.java
+++ src/main/java/org/wladska/Main.java
@@ -4,4 +4,93 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
+
+	/**
+	 * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+	 * Symbol       Value
+	 * I             1
+	 * V             5
+	 * X             10
+	 * L             50
+	 * C             100
+	 * D             500
+	 * M             1000
+	 * For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+	 * Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+	 * I can be placed before V (5) and X (10) to make 4 and 9. 
+	 * X can be placed before L (50) and C (100) to make 40 and 90. 
+	 * C can be placed before D (500) and M (1000) to make 400 and 900.
+	 * Given an integer, convert it to a roman numeral.
+	 * 
+	 * Example 1:
+	 * Input: num = 3
+	 * Output: "III"
+	 * 
+	 * Example 2:
+	 * Input: num = 58
+	 * Output: "LVIII"
+	 * 
+	 * Example 3:
+	 * Input: num = 1994
+	 * Output: "MCMXCIV"
+	 * 
+	 * Constraints:
+	 * 1 <= num <= 3999
+	 */
+	public String intToRoman(int num) {
+		StringBuilder sb = new StringBuilder();
+        while (num >= 1000) {
+            sb.append("M");
+            num -= 1000;
+        }
+        while (num >= 900) {
+            sb.append("CM");
+            num -= 900;
+        }
+        while (num >= 500) {
+            sb.append("D");
+            num -= 500;
+        }
+        while (num >= 400) {
+            sb.append("CD");
+            num -= 400;
+        }
+        while (num >= 100) {
+            sb.append("C");
+            num -= 100;
+        }
+		while (num >= 90) {
+            sb.append("XC");
+            num -= 90;
+        }
+		while (num >= 50) {
+            sb.append("L");
+            num -= 50;
+        }
+		while (num >= 40) {
+            sb.append("XL");
+            num -= 40;
+        }
+		while (num >= 10) {
+            sb.append("X");
+            num -= 10;
+        }
+		while (num >= 9) {
+            sb.append("IX");
+            num -= 9;
+        }
+		while (num >= 5) {
+            sb.append("V");
+            num -= 5;
+        }
+		while (num >= 4) {
+            sb.append("IV");
+            num -= 4;
+        }
+		while (num >= 1) {
+            sb.append("I");
+            num -= 1;
+        }
+		return sb.toString();
+	}
 }
