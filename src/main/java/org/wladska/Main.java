3c9d269c5ee4cb95267f6c336372cd8a32b4f768
Popularkiya <kingawladzinsk@gmail.com>
2024-03-19 14:38:51 +0100

    easy/base-7/copilot

--- src/main/java/org/wladska/Main.java
+++ src/main/java/org/wladska/Main.java
@@ -4,4 +4,36 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
+	/**
+	 * Given an integer num, return a string of its base 7 representation.
+	 * 
+	 * Example 1:
+	 * Input: num = 100
+	 * Output: "202"
+	 * 
+	 * Example 2:
+	 * Input: num = -7
+	 * Output: "-10"
+	 * 
+	 * Constraints:
+	 * -10^7 <= num <= 10^7
+	 */
+	public String convertToBase7(int num) {
+		if (num == 0) {
+			return "0";
+		}
+		StringBuilder sb = new StringBuilder();
+		boolean isNegative = num < 0;
+		if (isNegative) {
+			num = -num;
+		}
+		while (num > 0) {
+			sb.append(num % 7);
+			num /= 7;
+		}
+		if (isNegative) {
+			sb.append("-");
+		}
+		return sb.reverse().toString();
+	}
 }
