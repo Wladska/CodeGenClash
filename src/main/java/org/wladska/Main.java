fa8233a421101bc69f265d93283556bc837647c9
Popularkiya <kingawladzinsk@gmail.com>
2024-03-20 21:00:29 +0100

    medium/valid-sudoku/tabnine

--- src/main/java/org/wladska/Main.java
+++ src/main/java/org/wladska/Main.java
@@ -4,4 +4,59 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
+	/**
+	 * Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+	 * Each row must contain the digits 1-9 without repetition.
+	 * Each column must contain the digits 1-9 without repetition.
+	 * Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+	 * Note:
+	 * A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+	 * Only the filled cells need to be validated according to the mentioned rules.
+	 * 
+	 * Example 1:
+	 * Input: board = 
+	 * [["5","3",".",".","7",".",".",".","."]
+	 * ,["6",".",".","1","9","5",".",".","."]
+	 * ,[".","9","8",".",".",".",".","6","."]
+	 * ,["8",".",".",".","6",".",".",".","3"]
+	 * ,["4",".",".","8",".","3",".",".","1"]
+	 * ,["7",".",".",".","2",".",".",".","6"]
+	 * ,[".","6",".",".",".",".","2","8","."]
+	 * ,[".",".",".","4","1","9",".",".","5"]
+	 * ,[".",".",".",".","8",".",".","7","9"]]
+	 * Output: true
+	 * 
+	 * Example 2:
+	 * Input: board = 
+	 * [["8","3",".",".","7",".",".",".","."]
+	 * ,["6",".",".","1","9","5",".",".","."]
+	 * ,[".","9","8",".",".",".",".","6","."]
+	 * ,["8",".",".",".","6",".",".",".","3"]
+	 * ,["4",".",".","8",".","3",".",".","1"]
+	 * ,["7",".",".",".","2",".",".",".","6"]
+	 * ,[".","6",".",".",".",".","2","8","."]
+	 * ,[".",".",".","4","1","9",".",".","5"]
+	 * ,[".",".",".",".","8",".",".","7","9"]]
+	 * Output: false
+	 * 
+	 * Constraints:
+	 * board.length == 9
+	 * board[i].length == 9
+	 * board[i][j] is a digit 1-9 or '.'.
+	 */
+	public boolean isValidSudoku(char[][] board) {
+		for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    continue;
+                }
+                if (board[i][j]!= '1' && board[i][j]!= '2' && board[i][j]!= '3' && board[i][j]!= '4'
+                        && board[i][j]!= '5' && board[i][j]!= '6' && board[i][j]!= '7' && board[i][j]!= '8'
+                        && board[i][j]!= '9') {
+                    return false;
+                }
+            }
+        }
+        return true;
+	}
 }
