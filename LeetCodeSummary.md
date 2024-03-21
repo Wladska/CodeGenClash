28c45c86100b1ec3b8337cef36c537502e488214
Popularkiya <kingawladzinsk@gmail.com>
2024-03-21 21:37:55 +0100

    hard/wildcard-matching/code-whisperer

--- LeetCodeSummary.md
+++ LeetCodeSummary.md
@@ -1,6 +1,6 @@
 # CodeGenClash
 
-[ **place link to leet code after submit summary** ]
+https://leetcode.com/problems/wildcard-matching/submissions/1210344060
 
 ![runtime](./images/leetcodesummary/runtime.png)
 
--- src/main/java/org/wladska/Main.java
+++ src/main/java/org/wladska/Main.java
@@ -4,4 +4,30 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
+	/**
+	 * Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*' where:
+	 * '?' Matches any single character.
+	 * '*' Matches any sequence of characters (including the empty sequence).
+	 * The matching should cover the entire input string (not partial).
+	 * 
+	 * Example 1:
+	 * Input: s = "aa", p = "a"
+	 * Output: false
+	 * 
+	 * Example 2:
+	 * Input: s = "aa", p = "*"
+	 * Output: true
+	 * 
+	 * Example 3:
+	 * Input: s = "cb", p = "?a"
+	 * Output: false
+	 * 
+	 * Constraints:
+	 * 0 <= s.length, p.length <= 2000
+	 * s contains only lowercase English letters.
+	 * p contains only lowercase English letters, '?' or '*'.
+	 */
+	public boolean isMatch(String s, String p) {
+		return false;
+	}
 }
