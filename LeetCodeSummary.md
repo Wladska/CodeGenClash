078cfa7d26b54d0e02492192522b320aee46afbd
Popularkiya <kingawladzinsk@gmail.com>
2024-03-21 22:38:22 +0100

    hard/shortest-path-visiting-all-nodes/tabnine

--- LeetCodeSummary.md
+++ LeetCodeSummary.md
@@ -1,6 +1,6 @@
 # CodeGenClash
 
-[ **place link to leet code after submit summary** ]
+https://leetcode.com/problems/shortest-path-visiting-all-nodes/submissions/1210374792
 
 ![runtime](./images/leetcodesummary/runtime.png)
 
--- src/main/java/org/wladska/Main.java
+++ src/main/java/org/wladska/Main.java
@@ -4,4 +4,54 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
+	/**
+	 * You have an undirected, connected graph of n nodes labeled from 0 to n - 1. You are given an array graph where graph[i] is a list of all the nodes connected with node i by an edge.
+	 * Return the length of the shortest path that visits every node. You may start and stop at any node, you may revisit nodes multiple times, and you may reuse edges.
+	 * 
+	 * Example 1:
+	 * Input: graph = [[1,2,3],[0],[0],[0]]
+	 * Output: 4
+	 * 
+	 * Example 2:
+	 * Input: graph = [[1],[0,2,4],[1,3,4],[2],[1,2]]
+	 * Output: 4
+	 * 
+	 * Constraints:
+	 * n == graph.length
+	 * 1 <= n <= 12
+	 * 0 <= graph[i].length < n
+	 * graph[i] does not contain i.
+	 * If graph[a] contains b, then graph[b] contains a.
+	 * The input graph is always connected.
+	 */
+	 public int shortestPathLength(int[][] graph) {
+		 int n = graph.length;
+		 int[] dist = new int[n];
+		 for (int i = 0; i < n; i++) {
+             dist[i] = Integer.MAX_VALUE;
+         }
+		 dist[0] = 0;
+		 for (int i = 0; i < n; i++) {
+			 for (int j = 0; j < graph[i].length; j++) {
+                 if (dist[graph[i][j]] > dist[i] + 1) {
+                     dist[graph[i][j]] = dist[i] + 1;
+                 }
+             }
+			 if (dist[i] == Integer.MAX_VALUE) {
+                 return -1;
+             }
+
+
+			 for (int j = 0; j < graph[i].length; j++) {
+                 if (dist[graph[i][j]] > dist[i] + 1) {
+                     dist[graph[i][j]] = dist[i] + 1;
+                 }
+             }
+             if (dist[i] == Integer.MAX_VALUE) {
+                 return -1;
+             }
+         
+		  }
+          return dist[n - 1];
+	 }
 }
