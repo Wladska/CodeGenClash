2277aef97fe8848a405209b266db2a9480cb9598
Popularkiya <kingawladzinsk@gmail.com>
2024-03-22 13:37:44 +0100

    hard/count-vowels-permutation/code-whisperer

--- LeetCodeSummary.md
+++ LeetCodeSummary.md
@@ -1,6 +1,6 @@
 # CodeGenClash
 
-[ **place link to leet code after submit summary** ]
+https://leetcode.com/problems/count-vowels-permutation/submissions/1210860482
 
 ![runtime](./images/leetcodesummary/runtime.png)
 
--- src/main/java/org/wladska/Main.java
+++ src/main/java/org/wladska/Main.java
@@ -4,4 +4,38 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
+	/**
+	 * Given an integer n, your task is to count how many strings of length n can be formed under the following rules:
+	 * Each character is a lower case vowel ('a', 'e', 'i', 'o', 'u')
+	 * Each vowel 'a' may only be followed by an 'e'.
+	 * Each vowel 'e' may only be followed by an 'a' or an 'i'.
+	 * Each vowel 'i' may not be followed by another 'i'.
+	 * Each vowel 'o' may only be followed by an 'i' or a 'u'.
+	 * Each vowel 'u' may only be followed by an 'a'.
+	 * Since the answer may be too large, return it modulo 10^9 + 7.
+	 * 
+	 * Example 1:
+	 * Input: n = 1
+	 * Output: 5
+	 * 
+	 * Example 2:
+	 * Input: n = 2
+	 * Output: 10
+	 * 
+	 * Example 3: 
+	 * Input: n = 5
+	 * Output: 68
+	 * 
+	 * Constraints:
+	 * 1 <= n <= 2 * 10^4
+	 */
+	public int countVowelStrings(int n) {
+		int[][] dp = new int[n + 1][6];
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= 5; j++) {
+				dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+			}
+		}
+		return dp[n][5];
+	}
 }
