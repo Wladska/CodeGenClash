cc37fb72faa0770cf0d2c1387d0f0c20b31817d8
Popularkiya <kingawladzinsk@gmail.com>
2024-03-24 19:19:51 +0100

    sonar fixes

--- src/main/java/org/wladska/Main.java
+++ src/main/java/org/wladska/Main.java
@@ -1,5 +1,8 @@
 package org.wladska;
 
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
