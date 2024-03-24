5d80c17bc255f783d405d089aed2227766ab03c1
Popularkiya <kingawladzinsk@gmail.com>
2024-03-24 21:51:21 +0100

    sonar fixes

--- src/main/java/org/wladska/Main.java
+++ src/main/java/org/wladska/Main.java
@@ -31,6 +31,13 @@ public class Main {
 	 * 		ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 	 * 	}
 	 */
+	public class ListNode {
+	 int val;
+	 ListNode next;
+	 ListNode() {}
+	 ListNode(int val) { this.val = val; }
+	 ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	 }
 	public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 		if (list1 == null) {
             return list2;
