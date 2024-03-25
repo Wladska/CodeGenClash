de9fe8dced2a78fb20948de629f676a4cb49bb69
Popularkiya <kingawladzinsk@gmail.com>
2024-03-25 16:48:59 +0100

    easy/ugly-number/gpt-4

--- src/main/java/org/wladska/Main.java
+++ src/main/java/org/wladska/Main.java
@@ -4,4 +4,18 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
+
+	public boolean isUgly(int n) {
+		if (n <= 0) {
+			return false;
+		}
+		int[] factors = {2, 3, 5};
+		for (int factor : factors) {
+			while (n % factor == 0) {
+				n /= factor;
+			}
+		}
+		return n == 1;
+	}
+
 }
