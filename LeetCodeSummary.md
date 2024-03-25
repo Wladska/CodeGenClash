a70b6cc446b6f03fe1f7c982c29c94c36367b301
Popularkiya <kingawladzinsk@gmail.com>
2024-03-25 18:04:48 +0100

    medium/combinations/gpt-4

--- LeetCodeSummary.md
+++ LeetCodeSummary.md
@@ -1,6 +1,6 @@
 # CodeGenClash
 
-[ **place link to leet code after submit summary** ]
+https://leetcode.com/problems/combinations/submissions/1213652617
 
 ![runtime](./images/leetcodesummary/runtime.png)
 
--- src/main/java/org/wladska/Main.java
+++ src/main/java/org/wladska/Main.java
@@ -1,7 +1,36 @@
 package org.wladska;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
+
+	public List<List<Integer>> combine(int n, int k) {
+		List<List<Integer>> result = new ArrayList<>();
+		backtrack(result, new ArrayList<>(), 1, n, k);
+		return result;
+	}
+
+	private void backtrack(List<List<Integer>> result, List<Integer> combination, int start, int n, int k) {
+		// If the combination is done
+		if (k == 0) {
+			result.add(new ArrayList<>(combination));
+			return;
+		}
+
+		for (int i = start; i <= n; i++) {
+			// Add i into the current combination
+			combination.add(i);
+
+			// Move on to the next element
+			backtrack(result, combination, i + 1, n, k - 1);
+
+			// Backtrack
+			combination.remove(combination.size() - 1);
+		}
+	}
+
 }
