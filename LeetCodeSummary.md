f9e71ed333af9f60f7478433f3b75aab987bfbf2
Popularkiya <kingawladzinsk@gmail.com>
2024-03-25 17:56:12 +0100

    medium/valid-sudoku/gpt-4

--- LeetCodeSummary.md
+++ LeetCodeSummary.md
@@ -1,6 +1,6 @@
 # CodeGenClash
 
-[ **place link to leet code after submit summary** ]
+https://leetcode.com/problems/valid-sudoku/submissions/1213645992
 
 ![runtime](./images/leetcodesummary/runtime.png)
 
--- src/main/java/org/wladska/Main.java
+++ src/main/java/org/wladska/Main.java
@@ -1,7 +1,30 @@
 package org.wladska;
 
+import java.util.HashSet;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
+
+	public boolean isValidSudoku(char[][] board) {
+		HashSet<String> seen = new HashSet<>();
+
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				char number = board[i][j];
+				if (number != '.') {
+					if (!seen.add(number + " in row " + i) ||
+							!seen.add(number + " in column " + j) ||
+							!seen.add(number + " in block " + i / 3 + "-" + j / 3)) {
+						// If any of the add operations returns false, it means a duplicate was found
+						return false;
+					}
+				}
+			}
+		}
+
+		return true; // No duplicates were found
+	}
+
 }
