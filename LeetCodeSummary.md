1fd8b96a239ff1787c6b71cc9b893eb1d300c84c
Popularkiya <kingawladzinsk@gmail.com>
2024-03-25 18:57:12 +0100

    hard/n-queens/gpt-4

--- LeetCodeSummary.md
+++ LeetCodeSummary.md
@@ -1,6 +1,6 @@
 # CodeGenClash
 
-[ **place link to leet code after submit summary** ]
+https://leetcode.com/problems/n-queens/submissions/1213697235
 
 ![runtime](./images/leetcodesummary/runtime.png)
 
--- src/main/java/org/wladska/Main.java
+++ src/main/java/org/wladska/Main.java
@@ -1,7 +1,51 @@
 package org.wladska;
 
+import java.util.*;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
+
+	public List<List<String>> solveNQueens(int n) {
+		char[][] board = new char[n][n];
+		for (char[] row : board) {
+			Arrays.fill(row, '.');
+		}
+		List<List<String>> solutions = new ArrayList<>();
+		solve(0, n, board, solutions, new HashSet<>(), new HashSet<>(), new HashSet<>());
+		return solutions;
+	}
+
+	private void solve(int row, int n, char[][] board, List<List<String>> solutions,
+					   Set<Integer> cols, Set<Integer> diags, Set<Integer> antiDiags) {
+		if (row == n) {
+			solutions.add(construct(board));
+			return;
+		}
+		for (int col = 0; col < n; col++) {
+			if (cols.contains(col) || diags.contains(row - col) || antiDiags.contains(row + col)) {
+				continue;
+			}
+			cols.add(col);
+			diags.add(row - col);
+			antiDiags.add(row + col);
+			board[row][col] = 'Q';
+			solve(row + 1, n, board, solutions, cols, diags, antiDiags);
+			board[row][col] = '.';
+			cols.remove(col);
+			diags.remove(row - col);
+			antiDiags.remove(row + col);
+		}
+	}
+
+	private List<String> construct(char[][] board) {
+		List<String> solution = new ArrayList<>();
+		for (char[] row : board) {
+			String rowString = new String(row);
+			solution.add(rowString);
+		}
+		return solution;
+	}
+
 }
