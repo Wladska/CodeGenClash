d33fe46376be2a188e9d9cbfd1881857d5796b2c
Popularkiya <kingawladzinsk@gmail.com>
2024-03-25 18:38:43 +0100

    medium/gas-station/gpt-4

--- LeetCodeSummary.md
+++ LeetCodeSummary.md
@@ -1,6 +1,6 @@
 # CodeGenClash
 
-[ **place link to leet code after submit summary** ]
+https://leetcode.com/problems/gas-station/submissions/1213681633
 
 ![runtime](./images/leetcodesummary/runtime.png)
 
--- src/main/java/org/wladska/Main.java
+++ src/main/java/org/wladska/Main.java
@@ -4,4 +4,19 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
+
+	public int canCompleteCircuit(int[] gas, int[] cost) {
+		int totalTank = 0, currentTank = 0, startingStation = 0;
+		for (int i = 0; i < gas.length; i++) {
+			totalTank += gas[i] - cost[i];
+			currentTank += gas[i] - cost[i];
+			if (currentTank < 0) {
+				// Cannot reach station i + 1 from the current starting station
+				startingStation = i + 1;
+				currentTank = 0; // Reset the current tank
+			}
+		}
+		return totalTank >= 0 ? startingStation : -1;
+	}
+
 }
