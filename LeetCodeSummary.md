51a48c9ae0eade943f4ffb3d49bc50274f6376e1
Popularkiya <kingawladzinsk@gmail.com>
2024-03-25 17:20:21 +0100

    medium/task-scheduler/gpt-4)

--- LeetCodeSummary.md
+++ LeetCodeSummary.md
@@ -1,6 +1,6 @@
 # CodeGenClash
 
-[ **place link to leet code after submit summary** ]
+https://leetcode.com/problems/task-scheduler/submissions/1213602171?envType=daily-question&envId=2024-03-19
 
 ![runtime](./images/leetcodesummary/runtime.png)
 
--- src/main/java/org/wladska/Main.java
+++ src/main/java/org/wladska/Main.java
@@ -4,4 +4,29 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
+
+	public int leastInterval(char[] tasks, int n) {
+		int[] frequencies = new int[26];
+		for (char task : tasks) {
+			frequencies[task - 'A']++;
+		}
+
+		int maxFrequency = 0;
+		for (int f : frequencies) {
+			maxFrequency = Math.max(maxFrequency, f);
+		}
+
+		int tasksWithMaxFrequency = 0;
+		for (int f : frequencies) {
+			if (f == maxFrequency) tasksWithMaxFrequency++;
+		}
+
+		// Part 1: Calculate the part contributed by the most frequent tasks
+		int intervalsForMostFrequentTasks = (maxFrequency - 1) * (n + 1) + tasksWithMaxFrequency;
+
+		// Part 2: The total intervals must at least be the number of tasks
+		// because you can't complete tasks in fewer intervals than the number of tasks.
+		return Math.max(intervalsForMostFrequentTasks, tasks.length);
+	}
+
 }
