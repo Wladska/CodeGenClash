06f84d7540cc039c6726a67fd45502064007bcbc
Popularkiya <kingawladzinsk@gmail.com>
2024-03-25 16:42:38 +0100

    easy/contains-duplicate/gpt-4

--- src/main/java/org/wladska/Main.java
+++ src/main/java/org/wladska/Main.java
@@ -1,7 +1,23 @@
 package org.wladska;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+	}
+
+	public boolean containsDuplicate(int[] nums) {
+		Set<Integer> seen = new HashSet<>();
+		for (int num : nums) {
+			if (seen.contains(num)) {
+				// Found a duplicate
+				return true;
+			}
+			seen.add(num);
+		}
+		// No duplicates found
+		return false;
 	}
 }
