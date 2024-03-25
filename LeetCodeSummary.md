d9591d6b2fc13b1adec037b530b68e2642821adf
Popularkiya <kingawladzinsk@gmail.com>
2024-03-25 16:45:29 +0100

    easy/valid-anagram/gpt-4

--- LeetCodeSummary.md
+++ LeetCodeSummary.md
@@ -1,6 +1,6 @@
 # CodeGenClash
 
-[ **place link to leet code after submit summary** ]
+https://leetcode.com/problems/valid-anagram/submissions/1213587323
 
 ![runtime](./images/leetcodesummary/runtime.png)
 
--- src/main/java/org/wladska/Main.java
+++ src/main/java/org/wladska/Main.java
@@ -4,4 +4,27 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
+
+	public boolean isAnagram(String s, String t) {
+		if (s.length() != t.length()) {
+			return false;
+		}
+
+		int[] letterCounts = new int[26]; // For 'a' to 'z'
+
+		for (int i = 0; i < s.length(); i++) {
+			letterCounts[s.charAt(i) - 'a']++; // Increment count for 's'
+			letterCounts[t.charAt(i) - 'a']--; // Decrement count for 't'
+		}
+
+		for (int count : letterCounts) {
+			if (count != 0) {
+				// If any count is not zero, then 's' and 't' are not anagrams
+				return false;
+			}
+		}
+
+		return true; // All counts are zero, 's' and 't' are anagrams
+	}
+
 }
